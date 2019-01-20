caae7198420d74f961a8ac24a96abfcc9410c979
hsyxxi <81767375@qq.com>
2019-01-20 17:53:13 +0800

    19.1.20

--- index/js/index.js
+++ index/js/index.js
@@ -26,12 +26,6 @@ $(".footLogoWx").mouseover(function (){
     $(".footLogoWxDown").hide();  
  }); 
 
- /*$(".headDown").mouseover(function (){ 
-    $("").show();  
- }).mouseout(function (){  
-    $("").hide();  
- }); */
-
 function getDownPic(x,y){
 $(x).mouseover(function (){ 
     $(".downCode").show(),$(y).show();
@@ -42,3 +36,26 @@ $(x).mouseover(function (){
 getDownPic("#footDownDarkIos","#codeIos");
 getDownPic("#footDownDarkAnd","#codeAndroid");
 
+//圆点移动并放大效果
+function getMove(n,m,k){
+$(n).mouseover(function (){ 
+   $(m).css("left","55px"),
+   $(k).css("right","55px");
+}).mouseout(function (){  
+   $(m).css("left","45px"),
+   $(k).css("right","45px");
+}); 
+}
+getMove(".whyUsPic1",".whyUsPicL>.whyUsPicDotL",".whyUsPicL>.whyUsPicDotR");
+getMove(".whyUsPic2",".whyUsPicC>.whyUsPicDotL",".whyUsPicC>.whyUsPicDotR");
+getMove(".whyUsPic3",".whyUsPicR>.whyUsPicDotL",".whyUsPicR>.whyUsPicDotR");
+
+//开始GO的移动变色效果
+$(".startText").mouseover(function (){ 
+   $(".changeColor").css("width","100%"),
+   $(".startText>p").css("left","47%");
+   
+}).mouseout(function (){  
+   $(".changeColor").css("width","0%");
+   $(".startText>p").css("left","42%");
+}) 
